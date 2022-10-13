39bb3b3c8fc66440cf637c81056cc02716073802
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-10-13 18:46:09 +0100

    Add project files.

---
+++ UnicodeProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace UnicodeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("eureka.txt", "Eureka!");
+            byte[] eurekaBytes = File.ReadAllBytes("eureka.txt");
+            foreach (byte b in eurekaBytes)
+            {
+                Console.Write("{0} ", b);
+            }
+            Console.WriteLine(Encoding.UTF8.GetString(eurekaBytes));
+        }
+    }
+}
